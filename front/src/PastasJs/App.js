cede881ded64cd33e3d52908fc5e9a94cc91b578
Matheus Felipe <mfelipeleite8@gmail.com>
2024-05-05 18:53:13 -0300

    entrega03(front):finaliza interface grafica / atualiza o front corretamente

--- front/src/PastasJs/App.js
+++ front/src/PastasJs/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Aboutus from "./front/src/Aboutus";
-import Whatiswherever from "./front/src/Whatiswherever";
+import Aboutus from "./Aboutus";
+import Whatiswherever from "./Whatiswherever";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
