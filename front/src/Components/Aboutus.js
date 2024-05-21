fffdbaf5a5401461e9f197868022b7305130b7db
Matheus Felipe Leite Januario - 823117058 <823117058@ulife.com.br>
2024-05-21 19:47:54 -0300

    entrega03(front):finaliza interface gráfica
    os components foram editados da maneira correta, e futuras construções

--- front/src/Components/Aboutus.js
+++ front/src/Components/Aboutus.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-export default function Aboutus(){
+function Aboutus(){
     return(
         <div>
-            <h2>Cabeçalho</h2>
-        </div>
+            <h1/>teste<h1/>
+
+             <aside id="about-us-section"/>
+
+        <h1>Como a Wherever pode te ajudar?</h1>
+
+        <p class="paragrafo">
+            
+          Com Wherever, você descobre lugares incríveis com apenas um toque. 
+          Seja bares aconchegantes, restaurantes requintados, baladas vibrantes ou rooftops com vistas deslumbrantes, tudo está ao seu alcance. 
+          Defina seu raio de busca e deixe-nos revelar os tesouros escondidos ao seu redor!
+        </p>
+      </div>
     )
 }
+export default Aboutus;
